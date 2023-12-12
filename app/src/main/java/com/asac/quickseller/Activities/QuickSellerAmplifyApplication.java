75bf866c5e14ad926613bd1bf609f1bb00d76d14
hayahas <103423497+hayahas@users.noreply.github.com>
2023-12-12 16:30:48 +0300

    s3 done

--- app/src/main/java/com/asac/quickseller/Activities/QuickSellerAmplifyApplication.java
+++ app/src/main/java/com/asac/quickseller/Activities/QuickSellerAmplifyApplication.java
@@ -7,6 +7,7 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class QuickSellerAmplifyApplication extends Application {
 
@@ -19,6 +20,7 @@ public class QuickSellerAmplifyApplication extends Application {
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
             Amplify.configure(getApplicationContext());
         } catch (AmplifyException ae) {
             Log.e(TAG, "Error initializing Amplify" + ae.getMessage(), ae);
