f8810f056853fe5d0bf2470b1f8c8d2b0452f60e
mosubborini2000 <mosubborini2000@gmail.com>
2023-12-13 18:58:12 +0000

    postDetails

--- app/src/main/java/com/asac/quickseller/Activities/ItemDetailsActivity.java
+++ app/src/main/java/com/asac/quickseller/Activities/ItemDetailsActivity.java
@@ -2,7 +2,10 @@ package com.asac.quickseller.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.asac.quickseller.R;
 
@@ -12,5 +15,29 @@ public class ItemDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_details_activity);
+
+        Intent intent = getIntent();
+        String postId = intent.getStringExtra("postId");
+
+        String title = intent.getStringExtra("title");
+        String description = intent.getStringExtra("description");
+        String price = intent.getStringExtra("price");
+        String productCategory = intent.getStringExtra("productCategory");
+        String[] images = intent.getStringArrayExtra("images");
+        String city = intent.getStringExtra("city");
+
+
+
+        TextView titleTextView = findViewById(R.id.itemDetailsItemNameTextView);
+        TextView cityTextView = findViewById(R.id.itemDetailsItemCity);
+        TextView descriptionTextView = findViewById(R.id.itemDetailsItemDescription);
+        TextView priceTextView = findViewById(R.id.itemDetailsItemPrice);
+//        ImageView imageView = findViewById(R.id.itemDetailsImageView);
+
+        cityTextView.setText(city);
+        titleTextView.setText(title);
+        descriptionTextView.setText(description);
+        priceTextView.setText(price);
+
     }
 }
--- app/src/main/java/com/asac/quickseller/adapter/MyAdapter.java
+++ app/src/main/java/com/asac/quickseller/adapter/MyAdapter.java
@@ -1,21 +1,27 @@
 package com.asac.quickseller.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.amplifyframework.datastore.generated.model.Post;
+import com.asac.quickseller.Activities.ItemDetailsActivity;
 import com.asac.quickseller.R;
 
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
+
     Context context;
     List<Post> items;
+
 
     public MyAdapter(Context context, List<Post> items) {
         this.context = context;
@@ -25,14 +31,37 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.all_post_view , parent , false));
+        View view = LayoutInflater.from(context).inflate(R.layout.all_post_view, parent, false);
+        return new MyViewHolder(view);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Post post = items.get(position);
+
         holder.nameView.setText(items.get(position).getTitle());
         holder.priceView.setText(items.get(position).getPrice());
         holder.imageView.setImageResource(items.get(position).getImages().indexOf(0)); /////////////
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ItemDetailsActivity.class);
+                intent.putExtra("postId", post.getId());
+                intent.putExtra("city", post.getCity());
+                intent.putExtra("title", post.getTitle());
+                intent.putExtra("description", post.getDescription());
+                intent.putExtra("price", post.getPrice());
+                intent.putExtra("productCategory", post.getProductCategory());
+                intent.putExtra("images", post.getImages().toArray(new String[0]));
+
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
