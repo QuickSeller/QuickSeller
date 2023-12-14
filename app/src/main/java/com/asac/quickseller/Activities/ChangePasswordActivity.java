3af14c8542906e406305d1a4ef39cc4f982c3c5b
MDarwish993 <m.darwish993@hotmail.com>
2023-12-14 10:42:09 +0000

    mohammadDarwishDesignV02

---
+++ app/src/main/java/com/asac/quickseller/Activities/ChangePasswordActivity.java
@@ -0,0 +1,56 @@
+package com.asac.quickseller.Activities;
+
+import static com.amplifyframework.core.Amplify.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.asac.quickseller.R;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+
+
+        EditText editTextOldPassword = findViewById(R.id.editTextOldPassword);
+        EditText editTextNewPassword = findViewById(R.id.editTextNewPassword);
+        EditText editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        Button buttonChangePassword = findViewById(R.id.buttonChangePassword);
+
+        buttonChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String oldPassword = editTextOldPassword.getText().toString();
+                String newPassword = editTextNewPassword.getText().toString();
+                String confirmPassword = editTextConfirmPassword.getText().toString();
+
+                if (newPassword.equals(confirmPassword)) {
+                    Auth.updatePassword(
+                            oldPassword,
+                            newPassword,
+                            () -> runOnUiThread(() -> {
+                                Toast.makeText(ChangePasswordActivity.this, "Password changed successfully", Toast.LENGTH_SHORT).show();
+                                // Handle success scenario
+                            }),
+                            error -> runOnUiThread(() -> {
+                                Toast.makeText(ChangePasswordActivity.this, "Error changing password: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                                // Handle error scenario
+                            })
+                    );
+                } else {
+                    Toast.makeText(ChangePasswordActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
